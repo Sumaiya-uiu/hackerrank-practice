99c76f2d5e28b1e52c364f03f7265e152a0f3daa
Sumaiya <jahansumaiya49@gmail.com>
2025-08-09 22:43:01 +0600

    added explanation easy tuples

--- easy_tuples.py
+++ easy_tuples.py
@@ -1,13 +1,22 @@
+#Used python 2 for the expected result in the problem description. hash() produce different result in python 3.
+# python 2 solution
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split())
+    t = tuple(integer_list)
+    
+    print(hash(t))
+
 #Used python 2 for the expected result in the problem description. hash() produce different result in python 3
 
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-if __name__ == '__main__':
-    n = int(input())
-    l = []
-    i = 1
-    while i < n+1:
-        l.append(i)
-        i += 1
-    t = tuple(l)
+# Enter your code here. Read input from STDIN. Print output to STDOUT #python 3
+# if __name__ == '__main__':
+#     n = int(input())
+#     l = []
+#     i = 1
+#     while i < n+1:
+#         l.append(i)
+#         i += 1
+#     t = tuple(l)
 
-    print(hash(t))
+#     print(hash(t))
