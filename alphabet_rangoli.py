c64fc37e8898b05fa7a6e45b67717363b15d1e66
Sumaiya <jahansumaiya49@gmail.com>
2025-08-13 00:59:29 +0600

    Alphabet Rangoli easy problem

---
+++ alphabet_rangoli.py
@@ -0,0 +1,19 @@
+def print_rangoli(size):
+    alphabets = "abcdefghijklmnopqrstuvwxyz"
+    lines = []
+    for row in range(size):
+        print_ = "-".join(alphabets[row:size])
+        lines.append(print_[::-1] + print_[1:])
+    width = len(lines[0])
+    
+    for row in range(size-1, 0, -1):
+        print(lines[row].center(width, '-'))
+        
+    for row in range(size):
+        print(lines[row].center(width, '-'))
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
